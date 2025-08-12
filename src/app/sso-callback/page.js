97b257446ade8b35622e3e1db3835cbc643c042e
SakshamSharma10989 <skgharattal59@gmail.com>
2025-08-12 19:18:20 +0530

    done

--- src/app/sso-callback/page.js
+++ src/app/sso-callback/page.js
@@ -1,6 +1,5 @@
-// app/sso-callback/page.tsx
-import { SSOCallback } from '@clerk/nextjs/app-beta'
+import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SSOCallback />
+  return <AuthenticateWithRedirectCallback />;
 }
